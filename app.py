b1a4b629cd6b24dfe02dde1d6bc05d2fe1fde6e8
Rohit Ranjan <munnuranjanspiker@gmail.com>
2019-03-20 14:59:58 +0530

    create app.py and also templates directory

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import *
+app=Flask(__name__)
+@app.route("/")
+@app.route("/home")
+def home():
+	return render_template('home.html')
+@app.route("/about")
+def about():
+	return render_template('about.html')
