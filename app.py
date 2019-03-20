35390c78f7147b442675195dfe69c1d5d37973e4
Rohit Ranjan <munnuranjanspiker@gmail.com>
2019-03-21 04:03:56 +0530

    add some changes

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import *
 app=Flask(__name__)
 posts=[
 	{
-	'author' : 'rajiv ranjan',
-	'post_title': 'about pulwama',
-	'date_posted' : '21 apr 2019',
-	'content' : "pak terrorists attacked on indian army in pulwama at 31st feb"
-	},
+	'author' : 'rajiv ranjan',\
+	'post_title': 'about pulwama',\
+	'date_posted' : '21 apr 2019',\
+	'content' : 'pak terrorists attacked on indian army in pulwama at 31st feb'
+	},\
 	{
-	'author' : 'rohit ranjan',
-	'post_title' :'about air strike',
-	'date_posted' :'22 apr 2019',
+	'author' : 'rohit ranjan',\
+	'post_title' :'about air strike',\
+	'date_posted' :'22 apr 2019',\
 	'content' : 'in reply , india gives air strikes to pak terrorists'
 	}
 ]
