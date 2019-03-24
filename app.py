a0fc60204ca1b92792af85596f8c6ffad34dc965
Rohit Ranjan <munnuranjanspiker@gmail.com>
2019-03-24 16:25:22 +0530

    do some changes

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import *
 from forms import *
 from datetime import *
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user
+
 app=Flask(__name__)
 
 
@@ -10,7 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
 	user_id = db.Column(db.Integer, primary_key = True)
 	username = db.Column(db.String(20), unique = True , nullable = False)
 	email =  db.Column(db.String(120), unique = True , nullable = False)
@@ -29,6 +32,9 @@ class Post(db.Model):
 	def __repr__(self):
 		return f"POST('{self.title}','{self.date_posted}')"
 
+
+bcrypt = Bcrypt(app)
+
 posts=[
 	{
 	'author' : 'rajiv ranjan',\
@@ -43,19 +49,51 @@ posts=[
 	'content' : 'in reply , india gave air strikes to pak terrorists'
 	}
 ]
+
+
+
+login_manager = LoginManager(app)
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
+
+
+
 @app.route("/home")
 def home():
-	form = AboutForm()
-	return render_template('home.html',posts=posts,form= form)
+	form1 = AboutForm()
+	form2 = LogoutForm()
+	return render_template('home.html',posts=posts,form1= form1, form2= form2)
 @app.route("/about/")
 def about():
 	return render_template('about.html')
 @app.route('/register',methods=['GET','POST'])
 def register():
+	print("hello")
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		flash(f'account created successfully for {form.username.data}!','success')
-		return redirect(url_for('login'))
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+		check_username = User.query.filter_by(username = user.username).first()
+		check_email = User.query.filter_by(email = user.email).first()
+		if check_username == None and check_email == None:
+			db.session.add(user)
+			db.session.commit()
+			flash(f'account has been created successfully for {form.username.data}!','success')
+			return redirect(url_for('login'))
+		else :
+			if check_username :
+				error1 = 'username already taken'
+				print(error1)
+			else :
+				error1 = None
+			if check_email :
+				error2 = 'email already taken'
+				print(error2)
+			else : 
+				error2= None
+			
+			return render_template('register.html',title='register',form=form,error1 = error1,error2 = error2)
 
 	return render_template('register.html',title='register',form=form)
 @app.route('/login',methods=['GET','POST'])
@@ -63,8 +101,14 @@ def register():
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash(f'logged in successfully {form.email.data}!','success')
-		return redirect(url_for('home'))
+		check_existence = User.query.filter_by(email = form.email.data).first()
+		if check_existence and bcrypt.check_password_hash(check_existence.password,form.password.data):
+			check_existence.id = check_existence.user_id
+			login_user(check_existence, remember = False)
+			flash(f'logged in successfully {form.email.data}!','success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login unsuccessful. Please check your email and password that you entered','danger')
 	return render_template('login.html',title='login',form=form)
 if __name__=='__main__':
 	app.run(debug=True)
